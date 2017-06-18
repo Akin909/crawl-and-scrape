b53381e8988e8a0b2fed0077e85bec95f284de3e
Akin909 <akin.sowemimo@gmail.com>
2017-06-19 00:16:11 +0100

    remove cors plugin

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import cors from 'cors';
 import bodyParser from 'body-parser';
 import { getScrapings } from './scraper';
 
 const app = express();
 const PORT = process.env.PORT || 3005;
-
-app.use('*', cors({ origin: 'http://localhost:3000' }));
 
 //app.get('/crawl', getCrawling());
 
@@ -15,5 +12,5 @@ app.get('/scrapings', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Its going on!! at port ${PORT}`);
+  console.log(`Its going on, on port ${PORT}`);
 });
